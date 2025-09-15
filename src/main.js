25243a0878f0ff87688d0f618a9dc26c294aa03a
Marco Rehberg <marcorehberg@gmx.net>
2025-09-15 21:01:50 +0200

    Search Modal

--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
 import "./style.css";
+
+// Dialog Modal:
+const btnSearchDialog = document.getElementById("btnSearchDialog");
+const dialog = document.getElementById("meinDialog");
+const btnCloseDialog = document.getElementById("btnCloseDialog");
+
+btnSearchDialog.addEventListener("click", () => {
+  console.log("dialog");
+  dialog.showModal(); // Öffnet den Dialog modal
+});
+
+btnCloseDialog.addEventListener("click", () => {
+  dialog.close(); // Öffnet den Dialog modal
+});
