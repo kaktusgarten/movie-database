b49d001a5c7edd7c32e319173baf2eb10c639669
Marco Rehberg <marcorehberg@gmx.net>
2025-09-16 17:14:12 +0200

    Hero Input Field und Dialog window

--- src/main.js
+++ src/main.js
@@ -4,15 +4,25 @@ import { getTrendingMovies } from "./trending.js";
 getTrendingMovies();
 
 // Dialog Modal:
-const btnSearchDialog = document.getElementById("btnSearchDialog");
+// Der Button zum öffnen des Notiz-Dialog-Feldes muss so heißen, oder hier anpassen:
+const btnEnterNotizDialog = document.getElementById("btnEnterNotizDialog");
 const dialog = document.getElementById("meinDialog");
 const btnCloseDialog = document.getElementById("btnCloseDialog");
 
-btnSearchDialog.addEventListener("click", () => {
+btnEnterNotizDialog.addEventListener("click", (e) => {
   console.log("dialog");
   dialog.showModal(); // Öffnet den Dialog modal
 });
 
-btnCloseDialog.addEventListener("click", () => {
+btnCloseDialog.addEventListener("click", (e) => {
   dialog.close(); // Öffnet den Dialog modal
+});
+
+// SEARCH FORM
+const searchForm = document.getElementById("searchForm");
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const inputValue = e.target.querySelector("input[name='searchString']").value;
+  console.log(inputValue);
+  alert("Suche nach: " + inputValue);
 });
