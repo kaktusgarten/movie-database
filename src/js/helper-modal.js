7bb5cdded08c994a8ff025f269722b1503671cdc
yaaintmal <me@problack.de>
2025-09-17 21:50:23 +0200

    more refactoring

---
+++ src/js/helper-modal.js
@@ -0,0 +1,31 @@
+function showDialog(msg, mid) {
+  const dialog = document.createElement("dialog");
+  dialog.classList.add("show-dialog");
+  dialog.textContent = msg;
+
+  const buttonDiv = document.createElement("div");
+  buttonDiv.classList.add("button-container"); // tbd
+
+  // i dont care btn
+  const closeButton = document.createElement("button");
+  closeButton.textContent = "uninteressant";
+  closeButton.addEventListener("click", () => {
+    dialog.close();
+  });
+  buttonDiv.appendChild(closeButton);
+
+  // goto movie btn
+  const detailsButton = document.createElement("button");
+  detailsButton.textContent = "zum Film";
+  detailsButton.addEventListener("click", () => {
+    dialog.close();
+    window.location.href = `details.html?id=${mid}`;
+  });
+  buttonDiv.appendChild(detailsButton);
+  dialog.appendChild(buttonDiv);
+  document.body.appendChild(dialog);
+
+  dialog.showModal();
+}
+
+export { showDialog };
