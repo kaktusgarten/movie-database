07a5f286da11d70577ee98a968a8a5565a6621cb
yaaintmal <me@problack.de>
2025-09-18 18:45:38 +0200

    refactored

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ import "./static/style.css";
 import { getTrendingMovies } from "./js/trending.js";
 import { getSearchMovies } from "./js/search.js";
 
-if (window.location.pathname === "/") {
+if (
+  window.location.pathname === "/" ||
+  window.location.pathname === "/index.html"
+) {
   getTrendingMovies();
 }
 if (window.location.pathname === "/target-movie.html") {
